84c66561921bb40013a1b36fbc310b4da8383481
Cuckoometal <alex.fritzacus@gmail.com>
2016-02-08 10:04:24 -0700

    MOre work :(

---
+++ Pong/src/com/neumont/csc150/pong/model/Pong.java
@@ -0,0 +1,10 @@
+package com.neumont.csc150.pong.model;
+
+public class Pong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Pong/src/com/neumont/csc150/pong/view/MainPanel.java
@@ -0,0 +1,28 @@
+package com.neumont.csc150.pong.view;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel {
+
+	public MainPanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MainPanel(LayoutManager arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MainPanel(boolean arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MainPanel(LayoutManager arg0, boolean arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
